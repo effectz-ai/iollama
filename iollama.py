751d5b20f9653ee2cc0f466169079ee43ab11b17
λ.eranga <eranga.herath@pagero.com>
2024-03-24 15:13:42 -0400

    done with chroma

--- iollama.py
+++ iollama.py
@@ -1,60 +1,87 @@
+import chromadb
+import logging
+import sys
+
 from llama_index.llms.ollama import Ollama
-from llama_index.core import StorageContext
-from qdrant_client import QdrantClient
-from llama_index.vector_stores.qdrant import QdrantVectorStore
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from llama_index.core.settings import Settings
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext
-from llama_index.core import PromptTemplate
+from llama_index.core import (Settings, VectorStoreIndex, SimpleDirectoryReader, PromptTemplate)
+from llama_index.core import StorageContext
+from llama_index.vector_stores.chroma import ChromaVectorStore
+
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-llm = Ollama(
-    model="llama2",
-    request_timeout=300.0
-)
-Settings.llm = llm
-Settings.embed_model = HuggingFaceEmbedding(
-    model_name="BAAI/bge-small-en-v1.5"
-)
-
-# read documents from docs folder
-reader = SimpleDirectoryReader(
-    input_dir="./docs-red-team",
-    recursive=True,
-)
-docs = reader.load_data()
-print(f"Loaded {len(docs)} docs")
+llama_index.logger.level = "DEBUG"
 
 
-client = QdrantClient("http://localhost:6333")
-vector_store = QdrantVectorStore(
-    collection_name="iollama",
-    client=client,
-)
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
-index = VectorStoreIndex.from_documents(docs, storage_context=storage_context)
+def init_llm():
+    llm = Ollama(model="llama2", request_timeout=300.0)
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
-#query_engine = index.as_query_engine(streaming=True)
-#streaming_response = query_engine.query("what is chitra.")
-#streaming_response.print_response_stream()
+    Settings.llm = llm
+    Settings.embed_model = embed_model
 
-template = (
-    "We have provided context information below. \n"
-    "---------------------\n"
-    "{context_str}"
-    "\n---------------------\n"
-    "Given this information, please answer the question and each answer should start with code word AI Demos: {query_str}\n"
-)
-qa_template = PromptTemplate(template)
-query_engine = index.as_query_engine(text_qa_template=qa_template)
-#query_engine.update_prompts(
-#    {"response_synthesizer:text_qa_template": qa_template}
-#)
 
-while True:
-    input_question = input("Enter your question (or 'exit' to quit): ")
-    if input_question.lower() == 'exit':
-        break
+def init_index(embed_model):
+    reader = SimpleDirectoryReader(input_dir="./docs", recursive=True)
+    documents = reader.load_data()
 
-    response = query_engine.query(input_question)
-    print(response)
+    logging.info("index creating with `%d` documents", len(documents))
+
+    chroma_client = chromadb.EphemeralClient()
+    chroma_collection = chroma_client.create_collection("iollama")
+
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+    index = VectorStoreIndex.from_documents(documents, storage_context=storage_context, embed_model=embed_model)
+
+    return index
+
+
+def init_query_engine(index):
+    #index = VectorStoreIndex.from_vector_store(vector_store)
+
+    # custome template
+    template = (
+        "Imagine you are an advanced AI expert in cyber security laws, with access to all current and relevant legal documents, "
+        "case studies, and expert analyses. Your goal is to provide insightful, accurate, and concise answers to questions in this domain.\n\n"
+        "Here is some context related to the query:\n"
+        "-----------------------------------------\n"
+        "{context_str}\n"
+        "-----------------------------------------\n"
+        "Considering the above information, please respond to the following inquiry with detailed references to applicable laws, "
+        "precedents, or principles where appropriate:\n\n"
+        "Question: {query_str}\n\n"
+        "Answer succinctly, starting with the phrase 'According to cyber security law,' and ensure your response is understandable to someone without a legal background."
+    )
+    qa_template = PromptTemplate(template)
+
+    # build query engine with custom template
+    # text_qa_template specifies custom template
+    # similarity_top_k configure the retriever to return the top 3 most similar documents,
+    # the default value of similarity_top_k is 2
+    query_engine = index.as_query_engine(text_qa_template=qa_template, similarity_top_k=3)
+
+    return query_engine
+
+
+def chat(query_engine):
+    while True:
+        input_question = input("Enter your question (or 'exit' to quit): ")
+        if input_question.lower() == 'exit':
+            break
+
+        response = query_engine.query(input_question)
+
+        logging.info("got response from llm - %s", response)
+
+
+if __name__ == '__main__':
+    init_llm()
+    index = init_index(Settings.embed_model)
+    query_engine = init_query_engine(index)
+    chat(query_engine)
